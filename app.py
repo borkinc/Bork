6b48f19edec54503a5ba862411de8463b59b51fe
Christian Perez Villanueva <christian.perez34@upr.edu>
2019-02-01 14:54:29 -0400

    Added requirements. Made a minimal api.

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+users = {}
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class HelloWorld(Resource):
 
+    def get(self):
+        return {'hello': 'World'}
+
+
+class User(Resource):
+
+    def get(self, user_id):
+        return {user_id: users[user_id]}
+
+    def put(self, user_id):
+        users.update({user_id: request.form['data']})
+        return {user_id: users[user_id]}
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(User, '/<string:user_id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
