8c319f1ba8cd56e45678eae2119f6a4fc47413de
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-02-05 22:46:37 -0400

    Added CSS. Utilized boostrap to create login tab and sign-up tab.

--- react-frontend/src/UserAuth/UserAuth.js
+++ react-frontend/src/UserAuth/UserAuth.js
@@ -1,27 +1,51 @@
 import React, {Component} from 'react';
 import './UserAuth.css';
-import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+import {Button, Col, Form, FormGroup, Input, Label, Nav, NavItem, NavLink, Row, TabContent, TabPane} from "reactstrap";
 import axios from 'axios';
+import classnames from 'classnames';
 
 export default class UserAuth extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {username: ''}
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1',
+            username: '',
+            email: '',
+            password: ''
+        };
     }
 
-    handleChange = event => {
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
+
+    handleUsernameChange = event => {
         this.setState({username: event.target.value});
     };
 
-    handleSubmit = event => {
+    handlePasswordChange = event => {
+        this.setState({password: event.target.value});
+    };
+
+    handleEmailChange = event => {
+        this.setState({email: event.target.value});
+    };
+
+    handleLoginSubmit = event => {
         event.preventDefault();
 
-        // const user = {
-        //   username: this.state.username
-        // };
+        // Creating form to be sent to API
         const data = new FormData();
         data.append('username', this.state.username);
+        data.append('password', this.state.password);
+
+        // Contacting API to validate user password
         axios.post(`http://localhost:5000/api/user/1`, data, {
             headers: {'Content-Type': 'application/json',}
         })
@@ -33,31 +57,108 @@ export default class UserAuth extends Component {
 
 
     render() {
-        // const {username, password, submitted, loading, error} = this.state;
         return (
-            <div className="UserAuth">
-                <Form onSubmit={this.handleSubmit} className="login">
-                    <FormGroup>
-                        <Label for="username">Username</Label>
-                        <Input
-                            type="username"
-                            name="username"
-                            id="username"
-                            placeholder="Enter username"
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            id="examplePassword"
-                            placeholder="Enter password"
-                        />
-                    </FormGroup>
-                    <Button id="login-button">Login</Button>
-                </Form>
+            <div className="UserAuth-tabs">
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({active: this.state.activeTab === '1'})}
+                            onClick={() => {
+                                this.toggle('1');
+                            }}
+                        >
+                            Login
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({active: this.state.activeTab === '2'})}
+                            onClick={() => {
+                                this.toggle('2');
+                            }}
+                        >
+                            Sign-Up
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col sm="12">
+                                <div className="bork-logo">
+                                    <div className="UserAuth">
+                                        <Form onSubmit={this.handleLoginSubmit} className="login">
+                                            <FormGroup>
+                                                <Label for="username">Username</Label>
+                                                <Input
+                                                    type="username"
+                                                    name="username"
+                                                    id="UserAuth-username"
+                                                    placeholder="Enter username"
+                                                    onChange={this.handleUsernameChange}
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Label for="examplePassword">Password</Label>
+                                                <Input
+                                                    type="password"
+                                                    name="password"
+                                                    id="UserAuth-password"
+                                                    placeholder="Enter password"
+                                                    onChange={this.handlePasswordChange}
+                                                />
+                                            </FormGroup>
+                                            <Button>Login</Button>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                    <TabPane tabId="2">
+                        <Row>
+                            <Col sm="12">
+                                <div className="bork-logo">
+                                    <div className="UserAuth">
+                                        <Form onSubmit={this.handleLoginSubmit} className="login">
+                                            <FormGroup>
+                                                <Label for="username">Username</Label>
+                                                <Input
+                                                    type="username"
+                                                    name="username"
+                                                    id="UserAuth-username"
+                                                    placeholder="Enter username"
+                                                    onChange={this.handleUsernameChange}
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Label for="email">Email</Label>
+                                                <Input
+                                                    type="email"
+                                                    name="email"
+                                                    id="UserAuth-email"
+                                                    placeholder="Enter email"
+                                                    onChange={this.handleEmailChange}
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <Label for="examplePassword">Password</Label>
+                                                <Input
+                                                    type="password"
+                                                    name="password"
+                                                    id="UserAuth-password"
+                                                    placeholder="Enter password"
+                                                    onChange={this.handlePasswordChange}
+                                                />
+                                            </FormGroup>
+                                            <Button>Sign-Up</Button>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
             </div>
         );
     }
