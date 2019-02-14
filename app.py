a4f1350b55f9efe1272c4f5163aa45cd868cfcb4
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-02-13 22:32:46 -0400

    Restructuring restful API

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import bcrypt as bcrypt
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+api = Api(app, prefix='/api')
 
 DUMMY_DATA = {
     'register': {
@@ -16,9 +18,10 @@ DUMMY_DATA = {
 }
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class HelloWorld(Resource):
+
+    def get(self):
+        return {'hello': 'world'}
 
 
 @app.route('/api/register', methods=['POST'])
@@ -49,61 +52,92 @@ def login():
         return jsonify(user={'username': 'test'}, is_authenticated=True), 200
 
 
-@app.route('/api/chats', methods=['GET', 'POST'])
-def chats():
-    """
-    if method == GET
-    :return: All chats
+class Chats(Resource):
 
-    if method == POST
-    send user id and chat name
-    :return:
-    """
-    if request.method == 'GET':
+    def get(self):
         chats = [
             {"id": 1, "chat_name": "skiribops"},
             {"id": 2, "chat_name": "Subscribe to PewDiePie"},
             {"id": 3, "chat_name": "DB"}
         ]
-        return jsonify(results=chats), 200
+        return jsonify(chats=chats)
 
-    elif request.method == 'POST':
-        chat_name = request.form['chat_name']
-        owner_id = request.form['uid']
-        return jsonify(chat={'id': 2, 'chat_name': chat_name}, msg='Success'), 201
-
-
-@app.route('/api/chats/<int:cid>', methods=['GET'])
-def chatByID(cid):
-    if request.method == 'GET':
-        return jsonify(chat={'id': cid, 'chat_name': 'skiribops'}), 200
+    def post(self):
+        chat = {'id': 2, 'chat_name': 'Videout'}
+        return jsonify(chat=chat, msg='Success')
 
 
-@app.route('/api/contacts/<int:uid>', methods=['GET', 'POST'])
-def contacts(uid):
-    """
-    if GET
-    :param uid: user id de quien quieres la lista
-    :return:
+class Chat(Resource):
 
-    if POST
-    add a user to the contact del uid en el url
-    """
-    if request.method == 'GET':
-        return jsonify(contacts=[{'uid': 3}, {'uid': 4}]), 200
-    else:
-        uid_to_add = request.form['uid']
-        return jsonify(msg='added'), 201
+    def get(self, chat_id):
+        chat = {
+            "text": "This is a test message!",
+            "member": {
+                "id": 1
+            }
+        }
+        return jsonify(chat=chat)
 
 
-@app.route('/chats/<int:cid>/messages')
-def messages():
-    if request.method == 'GET':
-        return jsonify()
-    else:
-        # add message to chat
-        pass
+# @app.route('/api/chats', methods=['GET', 'POST'])
+# def chats():
+#     """
+#     if method == GET
+#     :return: All chats
+#
+#     if method == POST
+#     send user id and chat name
+#     :return:
+#     """
+#     if request.method == 'GET':
+#         chats = [
+#             {"id": 1, "chat_name": "skiribops"},
+#             {"id": 2, "chat_name": "Subscribe to PewDiePie"},
+#             {"id": 3, "chat_name": "DB"}
+#         ]
+#         return jsonify(results=chats), 200
+#
+#     elif request.method == 'POST':
+#         chat_name = request.form['chat_name']
+#         owner_id = request.form['uid']
+#         return jsonify(chat={'id': 2, 'chat_name': chat_name}, msg='Success'), 201
 
+
+# @app.route('/api/chats/<int:cid>', methods=['GET'])
+# def chatByID(cid):
+#     if request.method == 'GET':
+#         return jsonify(chat={'id': cid, 'chat_name': 'skiribops'}), 200
+#
+#
+# @app.route('/api/contacts/<int:uid>', methods=['GET', 'POST'])
+# def contacts(uid):
+#     """
+#     if GET
+#     :param uid: user id de quien quieres la lista
+#     :return:
+#
+#     if POST
+#     add a user to the contact del uid en el url
+#     """
+#     if request.method == 'GET':
+#         return jsonify(contacts=[{'uid': 3}, {'uid': 4}]), 200
+#     else:
+#         uid_to_add = request.form['uid']
+#         return jsonify(msg='added'), 201
+#
+#
+# @app.route('/chats/<int:cid>/messages')
+# def messages():
+#     if request.method == 'GET':
+#         return jsonify()
+#     else:
+#         # add message to chat
+#         pass
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Chats, '/chats')
+api.add_resource(Chat, '/chat/<int:chat_id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
