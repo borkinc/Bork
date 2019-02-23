4037985e571c0297d7feb386559286fa54be8712
javierbustillo <javier.bustillo@upr.edu>
2019-02-22 22:03:51 -0400

    Handlers for stats

--- Handlers/Chat.py
+++ Handlers/Chat.py
@@ -1,5 +1,7 @@
 import datetime
 
+from flask import jsonify
+
 
 class ChatHandler:
 
@@ -167,3 +169,28 @@ class ChatHandler:
             'replies': replies
         }
         return message
+
+    def get_trending_hashtags(self, request):
+        hashtags = [{'hid': 3, 'tag': '#bork'}, {'hid': 4, 'tag': '#borked'}]
+        return jsonify(result=hashtags)
+
+    def get_num_posts_daily(self, request):
+        return jsonify(result=4)
+
+    def get_num_likes_daily(self, request):
+        return jsonify(result=3)
+
+    def get_num_replies_daily(self, request):
+        return jsonify(result=1)
+
+    def get_num_dislikes_daily(self, request):
+        return jsonify(result=0)
+
+    def get_num_replies_photo(self, request):
+        return jsonify(result=1)
+
+    def get_num_likes_photo(self, request):
+        return jsonify(result=1)
+
+    def get_num_dislikes_photo(self, request):
+        return jsonify(result=100)
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
+from Handlers.Chat import ChatHandler
+from Handlers.Users import UserHandler
 from resources import UserRegistration, TokenRefresh, UserLogin, Chats, Index, ChatMessages, Contacts, Users, Chat, \
     LikeChatMessage, DislikeChatMessage, ReplyChatMessage
 
@@ -17,52 +19,52 @@ jwt = JWTManager(app)
 
 @app.route('/stats/trending')
 def trending_topics():
-    pass
+    return ChatHandler().get_trending_hashtags(request), 200
 
 
 @app.route('/stats/posts')
 def num_of_posts():
-    pass
+    return ChatHandler().get_num_posts_daily(request), 200
 
 
 @app.route('/stats/likes')
 def num_of_likes():
-    pass
+    return ChatHandler().get_num_likes_daily(request), 200
 
 
 @app.route('/stats/replies')
 def num_of_replies():
-    pass
+    return ChatHandler().get_num_replies_daily(request), 200
 
 
 @app.route('/stats/dislikes')
 def num_of_dislikes():
-    pass
+    return ChatHandler().get_num_dislikes_daily(request), 200
 
 
 @app.route('/stats/active')
 def active_users():
-    pass
+    return UserHandler().get_daily_active_users(request), 200
 
 
 @app.route('/stats/users/<int:uid>/messages')
 def num_of_mess_per_day(uid):
-    pass
+    return UserHandler().get_num_posts_user(request)
 
 
 @app.route('/stats/photos/<int:pid>/replies')
 def num_of_replies_photo(pid):
-    pass
+    return ChatHandler().get_num_replies_photo(request)
 
 
 @app.route('/stats/photos/<int:pid>/likes')
 def num_of_likes_photos(pid):
-    pass
+    return ChatHandler().get_num_likes_photo(request)
 
 
 @app.route('/stats/photos/<int:pid>/dislikes')
 def num_of_dislikes_photos(pid):
-    pass
+    return ChatHandler().get_num_dislikes_photo(request)
 
 
 api.add_resource(Index, '/')
