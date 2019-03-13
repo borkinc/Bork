52799a67c207cf9a2568ab2374b1b27bae49c216
javierbustillo <javier.bustillo@upr.edu>
2019-03-12 22:00:22 -0400

    New DB script, added UserDAO

--- DAO/UserDAO.py
+++ DAO/UserDAO.py
@@ -1,5 +1,4 @@
-import os
-from urllib.parse import urlparse
+
 
 import psycopg2
 
@@ -14,3 +13,11 @@ class UserDAO:
         self.conn = psycopg2.connect(dbname=config.DBNAME, user=config.USER, password=config.PASSWORD, host=config.HOST,
                                      port=config.PORT)
 
+    def insert_user(self, username, password, first_name, last_name, email, phone_number):
+        cursor = self.conn.cursor()
+        query = "insert into users (username, password, first_name, last_name, email, phone_number) " \
+                "values ('%s','%s','%s','%s','%s','%s') returning uid" % (username, password, first_name, last_name, email, phone_number)
+        cursor.execute(query)
+        uid = cursor.fetchone()[0]
+        self.conn.commit()
+        return uid
