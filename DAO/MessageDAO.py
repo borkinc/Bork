9e91a993faa1830cfbf32da62e484d307d10d8f8
javierbustillo <javier.bustillo@upr.edu>
2019-03-30 22:09:30 -0400

    Fixed queries where it had old table name for photo

--- DAO/MessageDAO.py
+++ DAO/MessageDAO.py
@@ -10,8 +10,8 @@ class MessageDAO(DAO):
         query = "with like_count as (select count(*) as likes, mid from likes where upvote = true group by mid), " \
                 "dislike_count as (select count(*) as dislikes, mid from likes where upvote = false group by mid) " \
                 "select message, image, likes, dislikes, username, messages.created_on from messages left outer join " \
-                "like_count on messages.mid = like_count.mid left outer join multimedia on " \
-                "messages.mid = multimedia.mid left outer join dislike_count on messages.mid = dislike_count.mid " \
+                "like_count on messages.mid = like_count.mid left outer join photo on " \
+                "messages.mid = photo.mid left outer join dislike_count on messages.mid = dislike_count.mid " \
                 "left outer join users on messages.uid = users.uid"
         cursor.execute(query)
         return cursor.fetchall()
@@ -21,8 +21,8 @@ class MessageDAO(DAO):
         query = "with like_count as (select count(*) as likes, mid from likes where upvote = true group by mid), " \
                 "dislike_count as (select count(*) as dislikes, mid from likes where upvote = false group by mid) " \
                 "select message, image, likes, dislikes, username, messages.created_on from messages left outer join " \
-                "like_count on messages.mid = like_count.mid left outer join multimedia on " \
-                "messages.mid = multimedia.mid left outer join dislike_count on messages.mid = dislike_count.mid " \
+                "like_count on messages.mid = like_count.mid left outer join photo on " \
+                "messages.mid = photo.mid left outer join dislike_count on messages.mid = dislike_count.mid " \
                 "left outer join users on messages.uid = users.uid where messages.mid = %s"
         cursor.execute(query, (mid,))
         return cursor.fetchall()
